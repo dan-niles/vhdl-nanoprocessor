f153991660625ec663995265f67fb9e6151d3e87
Dan Niles <niles_dan@live.com>
2022-07-17 11:38:14 +0530

    Import all modules into nanoprocessor

--- Lab10.srcs/sources_1/new/Nanoprocessor.vhd
+++ Lab10.srcs/sources_1/new/Nanoprocessor.vhd
@@ -38,9 +38,96 @@ entity Nanoprocessor is
 end Nanoprocessor;
 
 architecture Behavioral of Nanoprocessor is
+-- Slow Clock
 component Slow_Clk
-Port ( Clk_in : in STD_LOGIC;
-       Clk_out : out STD_LOGIC);
+    Port ( Clk_in : in STD_LOGIC;
+           Clk_out : out STD_LOGIC
+         );
+end component;
+
+-- Program Rom
+component Program_Rom
+    Port ( S : in STD_LOGIC_VECTOR (2 downto 0);
+           Q : out STD_LOGIC_VECTOR (0 to 11)
+         );
+end component;
+
+-- Program Counter
+component PC
+    Port ( D : in STD_LOGIC_VECTOR (2 downto 0);
+           Clk : in STD_LOGIC;
+           Res : in STD_LOGIC;
+           Q : out STD_LOGIC_VECTOR (2 downto 0)
+         );
+end component;
+
+-- Register Bank
+component Reg_Bank
+    Port ( D : in STD_LOGIC_VECTOR (3 downto 0);
+           I : in STD_LOGIC_VECTOR (2 downto 0);
+           Clk : in STD_LOGIC;
+           Clr : in STD_LOGIC;
+           R0 : out STD_LOGIC_VECTOR (3 downto 0);
+           R1 : out STD_LOGIC_VECTOR (3 downto 0);
+           R2 : out STD_LOGIC_VECTOR (3 downto 0);
+           R3 : out STD_LOGIC_VECTOR (3 downto 0);
+           R4 : out STD_LOGIC_VECTOR (3 downto 0);
+           R5 : out STD_LOGIC_VECTOR (3 downto 0);
+           R6 : out STD_LOGIC_VECTOR (3 downto 0);
+           R7 : out STD_LOGIC_VECTOR (3 downto 0)
+         );
+end component;
+
+-- 4 bit Add/Sub Unit
+component Add_Sub_Unit
+    Port ( A : in STD_LOGIC_VECTOR (3 downto 0);
+           B : in STD_LOGIC_VECTOR (3 downto 0);
+           Sel : in STD_LOGIC; -- Add/Sub Selector
+           S : out STD_LOGIC_VECTOR (3 downto 0);
+           C_Out : out STD_LOGIC; -- Carry flag
+           Z_Out : out STD_LOGIC; -- Zero flag
+           N_Out : out STD_LOGIC; -- Negetive flag
+           P_Out : out STD_LOGIC -- Parity flag (Odd parity detector)
+         );
+end component;
+
+-- 3 bit Adder
+component Add_3
+    Port ( A : in STD_LOGIC_VECTOR (2 downto 0);
+           S : out STD_LOGIC_VECTOR (2 downto 0)
+         );
+end component;
+
+-- 2-way 3-bit Mux
+component Mux_2_3
+    Port ( I0 : in STD_LOGIC_VECTOR (2 downto 0);
+           I1 : in STD_LOGIC_VECTOR (2 downto 0);
+           S : in STD_LOGIC;
+           Q : out STD_LOGIC_VECTOR (2 downto 0));
+end component;
+
+-- 2-way 4-bit Mux
+component Mux_2_4
+    Port ( I0 : in STD_LOGIC_VECTOR (3 downto 0);
+           I1 : in STD_LOGIC_VECTOR (3 downto 0);
+           S : in STD_LOGIC;
+           Q : out STD_LOGIC_VECTOR (3 downto 0)
+         );
+end component;
+
+-- 8-way 4-bit Mux
+component Mux_8_4
+    Port ( R0 : in STD_LOGIC_VECTOR (3 downto 0);
+           R1 : in STD_LOGIC_VECTOR (3 downto 0);
+           R2 : in STD_LOGIC_VECTOR (3 downto 0);
+           R3 : in STD_LOGIC_VECTOR (3 downto 0);
+           R4 : in STD_LOGIC_VECTOR (3 downto 0);
+           R5 : in STD_LOGIC_VECTOR (3 downto 0);
+           R6 : in STD_LOGIC_VECTOR (3 downto 0);
+           R7 : in STD_LOGIC_VECTOR (3 downto 0);
+           S : in STD_LOGIC_VECTOR (2 downto 0);
+           Q : out STD_LOGIC_VECTOR (3 downto 0)
+         );
 end component;
 
 signal Clk_slow : STD_LOGIC; -- Internal clock
